f45855fb507a53042d8244aacb1699a692bc39bd
Raju Saha <raj441977@gmail.com>
2021-06-19 13:36:07 +0530

    Update index.php

--- index.php
+++ index.php
@@ -256,7 +256,7 @@
             document.addEventListener("DOMContentLoaded", function(event) { 
                 //document.queryselector('body').style.zoom = 300%;
                 document.body.style['--zoom'] = '3';
-                document.queryselector('body').style.transform.scaleY = (.333333);
+                //document.queryselector('body').style.transform.scaleY = (.333333);
             });
         </script>
     </body>
