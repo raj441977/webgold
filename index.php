ae0595640a18101b1afed05ba5cb820515de16ea
Raju Saha <raj441977@gmail.com>
2021-06-19 14:52:59 +0530

    Update index.php

--- index.php
+++ index.php
@@ -253,14 +253,14 @@
                     mybody.msRequestFullscreen();
                 }
             }
-            document.addEventListener("DOMContentLoaded", function(event) { 
+            /*document.addEventListener("DOMContentLoaded", function(event) { 
                 //document.queryselector('body').style.zoom = 300%;
                 //document.body.style.zoom = '3';
                 //document.body.style.height = window.screen.height;
                 //document.queryselector('body').style.transform.scaleY = (.333333);
                 document.body.style.zoom = '3';
                 document.body.style.transform.scaleY = '0.333333';
-            });
+            });*/
         </script>
     </body>
 </html>
